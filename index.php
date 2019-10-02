2ff1b0b6041d78a25f7c155f90c835c556c04bb8
Sebastian Nilsson <nilsson.sebastian@ymail.com>
2019-10-02 18:29:27 +0200

    test deploy

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ require_once('model/RegisterModel.php');
 require_once('model/LoginModel.php');
 require_once('model/SessionModel.php');
 
-//MAKE SURE ERRORS ARE SHOWN... MIGHT WANT TO TURN THIS OFF ON A PUBLIC SERVER
+//MAKE SURE ERRORS ARE SHOWN.. MIGHT WANT TO TURN THIS OFF ON A PUBLIC SERVER
 error_reporting(E_ALL);
 ini_set('display_errors', 'On');
 

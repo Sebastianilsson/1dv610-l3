42814aeb265bd33d090b860fd3c5031449a30919
Sebastian Nilsson <nilsson.sebastian@ymail.com>
2019-10-20 15:39:48 +0200

    test

--- billboardComponent/model/Exceptions.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-class EmptyField extends Exception {}
-class HTMLTagsInText extends Exception {}
-class FailedConnection extends Exception {}
-class FailedToPrepareStatement extends Exception {}

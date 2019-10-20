4f617fe7100454ff2e83f3b8ce1dcf55a13b528f
Sebastian Nilsson <nilsson.sebastian@ymail.com>
2019-10-20 15:32:39 +0200

    adding Settings locally

--- Settings.php
+++ Settings.php
@@ -1,17 +1,19 @@
 <?php
 
-class Settings {
+class Settings
+{
     private $databaseServerName = 'localhost';
     private $databaseUserName = 'root';
     private $databasePassword = '';
     private $databaseName = '1dv610-l2';
 
-    public function __construct() {
+    public function __construct()
+    {
         $whitelist = array(
             '127.0.0.1',
             '::1'
         );
-        if(in_array($_SERVER['REMOTE_ADDR'], $whitelist)){
+        if (in_array($_SERVER['REMOTE_ADDR'], $whitelist)) {
             return;
         } else {
             $this->databaseServerName = getenv('DATABASE_SERVER_NAME');
@@ -21,19 +23,23 @@ class Settings {
         }
     }
 
-    public function getDatabaseServerName() {
+    public function getDatabaseServerName()
+    {
         return $this->databaseServerName;
     }
 
-    public function getDatabaseUsername() {
+    public function getDatabaseUsername()
+    {
         return $this->databaseUserName;
     }
 
-    public function getDatabasePassword() {
+    public function getDatabasePassword()
+    {
         return $this->databasePassword;
     }
 
-    public function getDatabaseName() {
+    public function getDatabaseName()
+    {
         return $this->databaseName;
     }
 }
